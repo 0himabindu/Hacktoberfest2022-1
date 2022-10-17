8c73ebb273accc3ba92848c0b495d7cd10c919df
Hima Bindu <96619687+0himabindu@users.noreply.github.com>
2022-10-17 12:52:52 +0530

    Add files via upload

--- linearsearch.cpp
+++ linearsearch.cpp
@@ -1,23 +1,34 @@
-#include<stdio.h>
-#include<conio.h>
-void main()
-{ 
-clrscr();
-   int a[ ]={ 2,3,4,5,1 },i,x,c,flag=1;
-printf(“ enter value to be searched … ”);
-scanf(“%d”,&x);
-   for(i=0;i<5,i++) 
-   { 
-if( a[i]==x)
-        flag=1;
-break ;
-       count=count+1;
-    }
+#include <iostream>
+  using namespace std;
 
- count=count+1;
-if(flag==1)
-printf(“ value found ”);
-else 
-printf(“ value not found ”);
-getch();
+void linearSearch(int a[], int n) {
+  int temp = -1;
+
+  for (int i = 0; i < 5; i++) {
+    if (a[i] == n) {
+      cout << "Element found at position: " << i + 1 << endl;
+      temp = 0;
+      break;
+    }
+  }
+
+  if (temp == -1) {
+    cout << "No Element Found" << endl;
+  }
+
+}
+
+int main() {
+  int arr[5];
+  cout << "Please enter 5 elements of the Array" << endl;
+  for (int i = 0; i < 5; i++) {
+    cin >> arr[i];
+  }
+  cout << "Please enter an element to search" << endl;
+  int num;
+  cin >> num;
+
+  linearSearch(arr, num);
+
+  return 0;
 }
